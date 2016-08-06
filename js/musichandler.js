313da5075aaa92ccb755e423bc26111f2962546e
Rayneel <Rayneel>
2016-08-06 14:21:35 +1200

    Add project files and folders

---
+++ js/musichandler.js
@@ -0,0 +1,7 @@
+var myClientId = "f74600b44fb330ca30bef5a71864d4bf";
+function initSC() {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
---
+++ js/musichandler.ts
@@ -0,0 +1,8 @@
+var myClientId = "f74600b44fb330ca30bef5a71864d4bf";
+
+function initSC() : void {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
